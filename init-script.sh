fcfe538ab69c18cb7bbb97a37ea2b619294a5c88
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-08-29 23:46:31 -0400

    config end

--- init-script.sh
+++ init-script.sh
@@ -7,7 +7,7 @@ apt-get install -y netcat curl postgresql-client >> /tmp/log.txt 2>&1
 
 # Ejecutar Odoo en segundo plano
 echo "Iniciando Odoo..."
-/usr/bin/odoo -r ${PGUSER} -w ${PGPASSWORD} --db_host ${PGHOST} --db_port ${PGPORT} -d ${PGDATABASE} >> /tmp/log.txt 2>&1 &
+/usr/bin/odoo -r ${PGUSER} -w ${PGPASSWORD} --db_host ${PGHOST} --db_port ${PGPORT} -d ${PGDATABASE} -i web_enterprise >> /tmp/log.txt 2>&1 &
 
 # Esperar a que Odoo esté completamente disponible
 echo "Esperando a que Odoo esté disponible..."
@@ -52,4 +52,4 @@ sleep 5
 
 # Reiniciar Odoo
 echo "Reiniciando Odoo..."
-/usr/bin/odoo -r ${PGUSER} -w ${PGPASSWORD} --db_host ${PGHOST} --db_port ${PGPORT} -d ${PGDATABASE}
+/usr/bin/odoo -r ${PGUSER} -w ${PGPASSWORD} --db_host ${PGHOST} --db_port ${PGPORT} -d ${PGDATABASE} -i web_enterprise
